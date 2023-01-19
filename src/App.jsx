2ed108ee7f493aa7413a304a21d987a604061eab
Eduardo <el_alienx@hotmail.com>
2023-01-19 10:26:20 +0100

    review and added icons (but there is a bug)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+// Node modules
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+
+// Project files
+import "./style.css";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Fontawesome Icons!</h1>
-      <FA icon="truck"/>
+      <FontAwesomeIcon icon={solid("square-check")} />
+
+      <button>
+        <FontAwesomeIcon icon={solid("coffee")} /> Buy me a coffee
+      </button>
     </div>
   );
 }
